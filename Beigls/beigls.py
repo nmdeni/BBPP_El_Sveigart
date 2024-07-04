10c36f78e96172f9ccd7bd243c05587ed91ed78a
nmdeni <mekadeni@mail.ru>
2024-07-04 12:02:21 +0500

    подготовил основной файл, написал описание игры и начала работать на основной функцией

---
+++ Beigls/beigls.py
@@ -0,0 +1,16 @@
+"""Бейглз, (c) Эл Свейгарт al@inventwithpython.com
+Дедуктивная логическая игра на угадывания числа по подсказкам.
+Код размещен на nostarch.com/big-book-small-python-projects
+Один из вариантов этой игры приведен в книге Invent Your Own
+Computer Games with Python на nostarch.com/inventwithpython
+Теги: короткая, игра, головоломка"""
+
+import random
+
+NUM_DIGITS = 3 # (!) Попробуйте задать эту константу равной 1 или 10
+MAX_GUESSES = 10 # (!) Попробуйте задать эту константу равной 1 или 100
+
+def main():
+    print('''PASS''')
+
+main()
