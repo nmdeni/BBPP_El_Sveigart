fae83a96f935ec70f13f532b84880bc75a993da4
nmdeni <mekadeni@mail.ru>
2024-08-08 17:47:07 +0500

    переписал функцию реализации списка дат для случайных дней рождения

--- Monte Karko/monte_karlo.py
+++ Monte Karko/monte_karlo.py
@@ -5,3 +5,17 @@ https://ru.wikipedia.org/wiki/Парадокс_дней_рождения
 Код размещен на https://nostarch.com/big-book-small-python-projects
 Теги: короткая, математическая, имитационное моделирование"""
 import datetime, random
+
+def getBirthdays(numberOfBirdays):
+    """Возвращем список объектов дат для случайных дней рождения."""
+    birtdays = []
+    for i in range(numberOfBirdays):
+        # год в нашем имитационном моделтрование роли не играет
+        # лишь бы в объектах дней рождения он был одинаков
+        startOfYear = datetime.date(2001,1,1)
+        # Получаем слуссайный день года
+        randomNumberOfDays = datetime.timedelta(random.randint(0,364))
+        birtday = startOfYear + randomNumberOfDays
+        birtdays.append(birtday)
+
+    return birtdays
