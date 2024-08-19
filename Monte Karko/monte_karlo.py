5641e7a909dc8d13f5a39059bf87beca863ac7b1
nmdeni <mekadeni@mail.ru>
2024-08-19 21:41:11 +0500

    переписал вывод информации

--- Monte Karko/monte_karlo.py
+++ Monte Karko/monte_karlo.py
@@ -32,4 +32,12 @@ def getMathc(birthdays):
             if birthdayA == birthdayB:
                 return birthdayA # возвращаем найденное соответствие
 
-#
+# отображения вводной информации
+print("""Birthday Paradox, by AI Sweigart ai@inventwithpython.com
+
+The Birthday Paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is serprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this comcept.
+
+(it's not actually a paradox, it's just a surprising result.)""")
